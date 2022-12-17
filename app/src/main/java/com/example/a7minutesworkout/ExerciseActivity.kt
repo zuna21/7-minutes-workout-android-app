c286334bc1ed5c3fa0ac2754c219ba9af6308d75
zuna21 <adir.zunic@gmail.com>
2022-12-17 15:01:09 +0100

    countDowntimer to display seconds remaining

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -2,12 +2,16 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +28,39 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        setupRestView()
+
+    }
+
+    private fun setRestProgressBar() {
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Rest time is finished",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun setupRestView() {
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
 
         binding = null
     }
